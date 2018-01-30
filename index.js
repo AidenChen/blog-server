6d0c285436956807eb716cfcc2cba9f44d09e4ac
Aiden <chenscrocker@hotmail.com>
2018-01-30 12:03:14 +0800

    新增https支持

--- index.js
+++ index.js
@@ -15,6 +15,9 @@ const convert = require('koa-convert')
 const onerror = require('koa-onerror')
 const cors = require('koa-cors')
 const bodyParser = require('koa-bodyparser')
+const enforceHttps = require('koa-sslify')
+const https = require('https')
+const fs = require('fs')
 const User = require('./app/Models/User')
 const Article = require('./app/Models/Article')
 const Tag = require('./app/Models/Tag')
@@ -23,6 +26,7 @@ const router = require('./routes')()
 const app = new Koa()
 // onerror(app)
 app
+  .use(enforceHttps())
   .use(convert(cors({
     expose: ['Content-Type', 'Authorization']
   })))
@@ -32,3 +36,10 @@ app
 app.listen(Config.app.port, () => {
   console.log('Listening: ' + Config.app.port)
 })
+
+const options = {
+  key: fs.readFileSync('/etc/nginx/cert/cert.key'),
+  cert: fs.readFileSync('/etc/nginx/cert/cert.pem')
+}
+
+https.createServer(options, app.callback()).listen(4433)
